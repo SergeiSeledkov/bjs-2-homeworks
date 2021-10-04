1d08889fdf658897b743d771bfe07e0da6928d96
SergeiSeledkov <gthemail92@gmail.com>
2021-10-04 18:45:34 +0500

    homework 2, v1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,74 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
+  let resultWorker;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++) {
+    resultWorker = func(arrOfArr[i]);
+
+    if (resultWorker > max) {
+      max = resultWorker;
+    }
+  }
   
-  return max
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let diffMinMax;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  min = Math.abs(min);
+  max = Math.abs(max);
+  if (min < max) {
+    diffMinMax = max - min;
+  } else {
+    diffMinMax = min - max;
+  }
+
+  return diffMinMax;
 }
