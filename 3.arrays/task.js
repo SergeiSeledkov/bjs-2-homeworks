2c524b4484b390a038b1dfed3015d503d35a7a2e
SergeiSeledkov <gthemail92@gmail.com>
2021-10-06 18:18:16 +0500

    homework 3, v1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = (arr1.length === arr2.length) ? arr1.every((value, index) => value === arr2[index]) : false;
 
   return result; // boolean
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(value => value > 0 && value % 3 === 0).map(value => value * 10);
 
   return resultArr; // array
 }
