ddb9774d5822d0e6265a4f4efbb1e49907f983bd
SergeiSeledkov <gthemail92@gmail.com>
2021-10-18 00:33:46 +0500

    homework 8, v1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,70 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  return function wrapper(...args) {
+    const hash = args.join(',');
+    let findIndexHash = cache.findIndex(item => item.hash === hash);
+
+    if (findIndexHash !== -1 ) {
+        return "Из кэша: " + cache[findIndexHash].value;
+    } else {
+        const result = func(...args);
+        cache.push({hash, value:result});
+
+        if (cache.length > 5) { 
+          cache.shift();
+        }
+
+        return "Вычисляем: " + result;  
+    }
+  }
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timeout = null;
+  let flag = false;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function wrapper() {
+    if (timeout === null) {
+      func();
+      flag = true;
+      timeout = setTimeout(() => flag = false, ms);
+    } else {
+      if (!flag) {
+        func();
+        flag = true;
+        timeout = setTimeout(() => flag = false, ms);
+      } else {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => flag = false, ms);
+      }
+    }
+  }
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+  let timeout = null;
+  let flag = false;
+  let workCount = 0;
+
+  function wrapper() {
+    wrapper.count = ++workCount;
+
+    if (timeout === null) {
+      func();
+      flag = true;
+      timeout = setTimeout(() => flag = false, ms);
+    } else {
+      if (!flag) {
+        func();
+        flag = true;
+        timeout = setTimeout(() => flag = false, ms);
+      } else {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => flag = false, ms);
+      }
+    }
+  }
+
+  return wrapper;
 }
